df78f5649e0e12215f264820b1a0aa2d9532e10c
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-02-21 20:38:15 +0200

    task-1 done

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,10 @@
   const listItems = document.querySelectorAll(".item");
 console.log(`Number of categories: ${listItems.length}`);
-  
+
+listItems.forEach(category => {
+  const categoryName = category.querySelector("h2").textContent;
+  console.log(`Category: ${categoryName}`);
+  const categoryQuantity = category.querySelectorAll("ul > li").length;
+  console.log(`Elements: ${categoryQuantity}`);
+});
   
